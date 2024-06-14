07eb4c8ddd30b2d309c6cb7170c46463127ab180
Melonpaan <151994801+Melonpaan@users.noreply.github.com>
2024-06-14 23:28:18 +0200

    IMP club.py
    j'ai effacé des méthodes inutilisées.

--- Application 2.0/classes/club.py
+++ Application 2.0/classes/club.py
@@ -37,13 +37,3 @@ class Club:
                 return cls.from_dict(data)
         return cls("Nom du Club", "Adresse du Club", "Président du Club")
 
-    def add_team(self, team_name):
-        if team_name not in self.teams:  # Ajouter une vérification pour éviter les doublons
-            self.teams.append(team_name)
-            self.save_to_file()  # Sauvegarder après l'ajout
-
-    def remove_team(self, team_name):
-        if team_name in self.teams:  # Vérifier si l'équipe existe avant de la supprimer
-            self.teams.remove(team_name)
-            self.save_to_file()  # Sauvegarder après la suppression
-
