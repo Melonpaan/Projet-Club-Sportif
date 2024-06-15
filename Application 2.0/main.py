4bf50df6c165b6076f78154087c00cbcf0681952
Melonpaan <151994801+Melonpaan@users.noreply.github.com>
2024-06-15 18:33:42 +0200

    DEL commentaires inutiles

--- Application 2.0/main.py
+++ Application 2.0/main.py
@@ -1,8 +1,5 @@
 from GUI.interface import GUIManager
 
 if __name__ == "__main__":
-    # Crée une instance de l'application GUIManager
     app = GUIManager()
-    
-    # Lance la boucle principale de l'application Tkinter
     app.mainloop()
