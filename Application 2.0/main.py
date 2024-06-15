5fb80d765848d170550837e377f681f40843f2eb
Melonpaan <151994801+Melonpaan@users.noreply.github.com>
2024-06-15 17:19:09 +0200

    ADD documentation main.py

--- Application 2.0/main.py
+++ Application 2.0/main.py
@@ -1,5 +1,8 @@
 from GUI.interface import GUIManager
 
 if __name__ == "__main__":
+    # Crée une instance de l'application GUIManager
     app = GUIManager()
+    
+    # Lance la boucle principale de l'application Tkinter
     app.mainloop()
